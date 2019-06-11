7d593e156b5fd3923eb3b2d7d6434da0d607b563
Jessica Munoz and Rochelle Roberts <jessica@>
2019-06-11 13:54:12 -0700

    prints word with dash symbol

--- js/scripts.js
+++ js/scripts.js
@@ -6,27 +6,46 @@ $(document).ready(function() {
     //get user Input
     var input = $("#stringInput").val();
     var charArray = [];
+    var vowel = ["a", "e", "i", "o", "u"];
+    //array for splitting
+    var splitArray = [];
+
+    splitArray= input.split("");
+
+    //alert(splitArray);
 
     for(index =0; index < input.length; index +=1){
       charArray[index] = input.charAt(index);
     }
-debugger;
-    for(index= 0; index < charArray.length; index+=1){
-      if(charArray[index] === 'a' || charArray[index] === 'e' || charArray[index] === 'i' || charArray[index] === 'o' || charArray[index] === 'u'){
-        alert("found vowel!");
 
-      }else {
-        alert("no vowel found");
+    for(index =0; index < splitArray.length; index+=1){
+      if(vowel.includes(splitArray[index])){
+        splitArray[index] = "-";
+        splitArray.join("");
+
+      }else{
+        // do nothing
       }
-
     }
+    $("#output").append(splitArray);
+    // this is the regEx way
+    // var newArray = input.replace(/e/, '-');
+    // alert(newArray)
+
+// debugger;
 
 
-
-
-
-
-
+// second method, not finished, prints with commas!
+    // for(index= 0; index < charArray.length; index+=1){
+    //   if(charArray[index] === 'a' || charArray[index] === 'e' || charArray[index] === 'i' || charArray[index] === 'o' || charArray[index] === 'u'){
+    //     charArray[index] = "-";
+    //     //alert(charArray);
+    //
+    //   }else {
+    //
+    //   }
+    //
+    // }
 
       event.preventDefault();
   });
